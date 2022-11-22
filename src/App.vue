c619135e2242025f4dc9282ccd58ae6a3867a0b3
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-22 19:58:02 -0300

    onDate - first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter"></div>
+    <SideBar />
+    <div class="column is-three-quarter"></div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import SideBar from "./components/SideBar/SideBar.vue";
+import Form from "./components/Form/Form.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    SideBar,
+    Form,
+  },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="Form">
+        <input type="text" class="input" placeholder="task" />
+      </div>
+      <div class="column">
+        <section><strong>00:00:00</strong></section>
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-play"></i>
+          </span>
+          <span>play</span>
+        </button>
+        <button class="button">
+          <span class="icon">
+            <i class="fas fa-stop"></i>
+          </span>
+          <span>stop</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Form",
+});
+</script>
