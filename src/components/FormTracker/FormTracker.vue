7f877136b2c350db23f116bbe264fbc52c737c75
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-25 14:32:37 -0300

    refact

--- src/components/FormTracker/FormTracker.vue
+++ src/components/FormTracker/FormTracker.vue
@@ -4,53 +4,19 @@
       <div class="column is-8" role="form">
         <input type="text" class="input" placeholder="task" />
       </div>
-      <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <Stopwatch :timeInSec="timeInSec" />
-          <button class="button" @click="start()">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finalize()">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
-      </div>
+      <div class="column"><Timer /></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Stopwatch from "../Stopwatch/Stopwatch.vue";
+import Timer from "../Timer/Timer.vue";
 
 export default defineComponent({
   name: "FormTracker",
   components: {
-    Stopwatch,
-  },
-  data() {
-    return {
-      timeInSec: 0,
-      timer: 0,
-    };
-  },
-  methods: {
-    start() {
-      this.timer = setInterval(() => {
-        this.timeInSec += 1;
-      }, 1000);
-    },
-    finalize() {
-      clearInterval(this.timer);
-    },
+    Timer,
   },
 });
 </script>
---
+++ src/components/Timer/Timer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Stopwatch :timeInSec="timeInSec" />
+    <button class="button" @click="start()">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalize()">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Stopwatch from "../Stopwatch/Stopwatch.vue";
+
+export default defineComponent({
+  name: "Timer",
+  components: {
+    Stopwatch,
+  },
+  data() {
+    return {
+      timeInSec: 0,
+      timer: 0,
+    };
+  },
+  methods: {
+    start() {
+      this.timer = setInterval(() => {
+        this.timeInSec += 1;
+      }, 1000);
+    },
+    finalize() {
+      clearInterval(this.timer);
+    },
+  },
+});
+</script>
