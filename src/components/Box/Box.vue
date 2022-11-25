8ab4ec070e6d75a7c9dc28c59eb95b60a7d98c12
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-25 15:48:45 -0300

    pattern

---
+++ src/components/Box/Box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box has-text-weight-bold"><slot></slot></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, DefineComponent } from "vue";
+
+export default defineComponent({
+  name: "Box",
+});
+</script>
+
+<style scoped>
+.box {
+  background: rgb(201, 201, 201);
+}
+</style>
