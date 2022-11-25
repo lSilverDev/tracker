5fa15128d4cae04c5896747cc5c43b61ab4a3685
Eduardo <67278780+lSilverDev@users.noreply.github.com>
2022-11-25 14:38:37 -0300

    rules

--- src/components/Timer/Timer.vue
+++ src/components/Timer/Timer.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Stopwatch :timeInSec="timeInSec" />
-    <button class="button" @click="start()">
+    <button class="button" @click="start()" :disabled="running">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finalize()">
+    <button class="button" @click="finalize()" :disabled="!running">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -29,15 +29,18 @@ export default defineComponent({
     return {
       timeInSec: 0,
       timer: 0,
+      running: false,
     };
   },
   methods: {
     start() {
+      this.running = true;
       this.timer = setInterval(() => {
         this.timeInSec += 1;
       }, 1000);
     },
     finalize() {
+      this.running = false;
       clearInterval(this.timer);
     },
   },
